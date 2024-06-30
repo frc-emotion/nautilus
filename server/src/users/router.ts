abd889c5311ad428df53b10e394ddb46aebe30ea
Jason Ballinger <jasonballinger_dev@yahoo.com>
2024-06-29 19:02:50 -0700

    meow

--- server/src/users/router.ts
+++ server/src/users/router.ts
@@ -5,27 +5,18 @@ const router = Router();
 
 // TODO: Need to protect
 
-// Login - working
-router.post("/login", routes.login);
-// Register - working
-router.post("/register", routes.register);
+// Auth
+router.post("/login", routes.login); // Login - working
+router.post("/register", routes.register); // Register - woring
+// router.post("/forgot"); // Forgot Password - not started
+// router.post("/reset"); // Reset Password - not started
 
-// Forgot Password
-// router.post("/forgot");
-// Reset Password
-// router.post("/reset");
-
-// Get Users - working, need to protect/limit based on access
-router.get("/", routes.getAllUsers);
-// Get User by Id - working, need to protect/limit based on access
-router.get("/:id", routes.getUserById);
-// Update self
-router.put("/", routes.updateProfile);
-// Update User by Id - SUPERUSER, fallback to self, or 403
-router.put("/:id", routes.updateUserAdmin);
-// Delete self
-router.delete("/", routes.deleteSelf);
-// Delete User by Id - woring, SUPERUSER ONLY, fallback to self, or 403
-router.delete("/:id", routes.deleteUser);
+// CRUD
+router.get("/", routes.getAllUsers); // Get Users - working, need to protect/limit based on access
+router.get("/:id", routes.getUserById); // Get User by Id - working, need to protect/limit based on access
+router.put("/", routes.updateProfile); // Update self
+router.put("/:id", routes.updateUserAdmin); // Update User by Id - SUPERUSER, fallback to self, or 403
+router.delete("/", routes.deleteSelf); // Delete self
+router.delete("/:id", routes.deleteUser); // Delete User by Id - woring, SUPERUSER ONLY, fallback to self, or 403
 
 module.exports = router;
