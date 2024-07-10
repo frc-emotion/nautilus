612a3047515d594cd42201b8381c1ab15c08ac7a
Jason Ballinger <jasonballinger_dev@yahoo.com>
2024-07-09 21:10:50 -0700

    shadcn...

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBr./App.jser, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
