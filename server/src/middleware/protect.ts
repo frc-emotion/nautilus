4f7327f9db7950c5191cd06e33da97ca0fac9833
Jason Ballinger <jasonballinger_dev@yahoo.com>
2024-07-22 20:22:26 -0700

    preliminary protect working

--- server/src/middleware/protect.ts
+++ server/src/middleware/protect.ts
@@ -1,8 +1,8 @@
-// import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 // import { Role } from "@prisma/client";
 // // import { Permissions } from "../types/permissions";
-// import { prisma } from "..";
-// import { Request, Response, NextFunction } from "express";
+import { prisma } from "..";
+import { Request, Response, NextFunction } from "express";
 
 // const summarizePermissions = (roles: Role[]): Permissions => {
 //     let summarized: Permissions = {};
@@ -36,64 +36,61 @@
 //     return true;
 // };
 
-// function protect(
-//     permission: Permissions,
-//     protecting: Function,
-//     fallback?: Function
-// ) {
-//     return async function (req: Request, res: Response, next: NextFunction) {
-//         let token;
+function protect(
+    // permission: Permissions,
+    protecting: Function,
+    fallback?: Function
+) {
+    return async function (req: Request, res: Response, next: NextFunction) {
+        let token;
+        if (req.cookies.token) {
+            token = req.cookies.token;
+        } else if (
+            req.headers.authorization &&
+            req.headers.authorization.startsWith("Bearer")
+        ) {
+            token = req.headers.authorization.split(" ")[1];
+        } else {
+            if (fallback) return fallback(req, res, next);
+            else return res.status(401).send({ message: "Unauthorized" });
+        }
 
-//         if (
-//             req.headers.authorization &&
-//             req.headers.authorization.startsWith("Bearer")
-//         ) {
-//             try {
-//                 // separate token from Bearer
-//                 token = req.headers.authorization.split(" ")[1];
+        try {
+            const decoded = jwt.verify(
+                token,
+                process.env.JWT_SECRET as string
+            ) as { id: string };
 
-//                 // verify jwt against secret
-//                 const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+            const user = await prisma.user.findUnique({
+                where: {
+                    id: decoded.id,
+                },
+                include: {
+                    roles: true,
+                },
+            });
 
-//                 // prisma user find equivalent
-//                 req.user = await prisma.user.findUniqueOrThrow({
-//                     where: {
-//                         id: (decoded as { id: string }).id,
-//                     },
-//                     include: {
-//                         roles: true,
-//                     },
-//                 });
-//             } catch (err) {
-//                 // console.log(err);
-//                 res.status(401).json({
-//                     message: "Not authorized, token failed",
-//                 });
-//             }
+            if (!user) {
+                if (fallback) return fallback(req, res, next);
+                else return res.status(403).send({ message: "Forbidden" });
+            }
 
-//             if (
-//                 comparePermissions(
-//                     permission,
-//                     summarizePermissions(req.user!.roles as Role[])
-//                 )
-//             ) {
-//                 // user has permission
-//                 protecting(req, res, next);
-//             } else if (fallback) {
-//                 // send user to fallback
-//                 fallback(req, res, next);
-//             } else {
-//                 // no fallback
-//                 res.status(403).json({
-//                     message: "Forbidden",
-//                 });
-//             }
-//         } else {
-//             res.status(401).json({
-//                 message: "No token",
-//             });
-//         }
-//     };
-// }
+            // const permissions = summarizePermissions(user.roles);
 
-// export default protect;
+            // if (!comparePermissions(permission, permissions)) {
+            //     if (fallback) return fallback(req, res, next);
+            //     else return res.status(403).send({ message: "Forbidden" });
+            // }
+
+            req.user = user;
+
+            return protecting(req, res, next);
+        } catch (error) {
+            return res
+                .status(500)
+                .send({ message: "Internal Server Error", dump: error });
+        }
+    };
+}
+
+export default protect;
