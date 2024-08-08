76412c6c2e4cb45a99aacc6aa7d14266f9a0be65
Jason Ballinger <jasonballinger_dev@yahoo.com>
2024-08-07 23:32:23 -0700

    consolidate directory table & allow user delete

--- dashboard/src/pages/directory/Directory.tsx
+++ dashboard/src/pages/directory/Directory.tsx
@@ -1,41 +1,13 @@
 import Navbar from "@/components/nav/Navbar";
 import PageTitle from "@/components/properties/PageTitle";
-import DirectoryTable, { columns } from "./DirectoryTable";
-import { useState, useEffect } from "react";
+import DirectoryTable from "./DirectoryTable";
 
 export default function Directory() {
-    const [userList, setUserList] = useState([]);
-
-    useEffect(() => {
-        async function fetchUsers() {
-            const response = await fetch(
-                `${import.meta.env.VITE_API_URL}/users`,
-                {
-                    method: "GET",
-                    credentials: "include",
-                }
-            );
-
-            const data = await response.json();
-            if (response.ok) {
-                setUserList(data.users);
-            } else {
-                // TODO: Handle error
-                console.error(data.message);
-            }
-        }
-
-        fetchUsers();
-    }, []);
-
     return (
         <>
             <PageTitle title="Directory" />
             <Navbar />
-            <DirectoryTable
-                data={userList}
-                columns={columns}
-            />
+            <DirectoryTable />
         </>
     );
 }
--- dashboard/src/pages/directory/DirectoryTable.tsx
+++ dashboard/src/pages/directory/DirectoryTable.tsx
@@ -24,81 +24,199 @@ import {
     TableRow,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import { Dialog, DialogTrigger, DialogContent } from "@/components/ui/dialog";
 import { UserNoPassword } from "@/context/auth";
-import { MoreHorizontal } from "lucide-react";
-import LogoutButton from "@/components/nav/LogoutButton";
-import UserAvatar from "@/components/user/UserAvatar";
+import { Loader2, MoreHorizontal } from "lucide-react";
 import { Link } from "react-router-dom";
+import { Dialog, DialogDescription } from "@radix-ui/react-dialog";
+import {
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog";
 
-interface DataTableProps<TData, TValue> {
+export default function DirectoryTable() {
+    const [data, setData] = useState<UserNoPassword[]>([]);
+
+    useEffect(() => {
+        async function fetchUsers() {
+            const response = await fetch(
+                `${import.meta.env.VITE_API_URL}/users`,
+                {
+                    method: "GET",
+                    credentials: "include",
+                }
+            );
+
+            const data = await response.json();
+            if (response.ok) {
+                setData(data.users);
+            } else {
+                // TODO: Handle error
+                console.error(data.message);
+            }
+        }
+
+        fetchUsers();
+    }, []);
+
+    async function handleUserDelete(id: string) {
+        const response = await fetch(
+            `${import.meta.env.VITE_API_URL}/users/${id}`,
+            {
+                method: "DELETE",
+                credentials: "include",
+            }
+        );
+
+        if (response.ok) {
+            console.log("User deleted");
+            setData((prevData) => prevData.filter((user) => user.id !== id));
+        } else {
+            console.error("Failed to delete user");
+        }
+    }
+
+    const columns: ColumnDef<UserNoPassword>[] = [
+        {
+            accessorKey: "firstname",
+            header: "First Name",
+        },
+        {
+            accessorKey: "lastname",
+            header: "Last Name",
+        },
+        {
+            accessorKey: "email",
+            header: "Email",
+        },
+        {
+            accessorKey: "phone",
+            header: "Phone",
+        },
+        {
+            id: "actions",
+            cell: ({ row }) => {
+                const user = row.original;
+                const [editUserOpen, setEditUserOpen] = useState(false);
+                const [confirmDeleteOpen, setConfirmDeleteOpen] =
+                    useState(false);
+                const [deleteBusy, setDeleteBusy] = useState(false);
+
+                return (
+                    <>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button variant="ghost" className="h-8 w-8 p-0">
+                                    <span className="sr-only">Open menu</span>
+                                    <MoreHorizontal className="h-4 w-4" />
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                                <DropdownMenuItem
+                                    onClick={() =>
+                                        navigator.clipboard.writeText(user.id)
+                                    }
+                                >
+                                    Copy User ID
+                                </DropdownMenuItem>
+                                <DropdownMenuSeparator />
+                                <Link to={`/user/${user.id}`}>
+                                    <DropdownMenuItem>
+                                        View Profile
+                                    </DropdownMenuItem>
+                                </Link>
+
+                                <DropdownMenuItem
+                                    onClick={() => {
+                                        setEditUserOpen(true);
+                                    }}
+                                >
+                                    Edit User
+                                </DropdownMenuItem>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem
+                                    className="font-bold text-red-600"
+                                    onClick={() => {
+                                        setConfirmDeleteOpen(true);
+                                    }}
+                                >
+                                    Delete
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                        <Dialog
+                            open={editUserOpen}
+                            onOpenChange={setEditUserOpen}
+                        >
+                            <DialogContent>meow</DialogContent>
+                        </Dialog>
+                        <Dialog
+                            open={confirmDeleteOpen}
+                            onOpenChange={setConfirmDeleteOpen}
+                        >
+                            <DialogContent>
+                                <DialogHeader>
+                                    <DialogTitle>Are you sure?</DialogTitle>
+                                    <DialogDescription>
+                                        This action cannot be reversed.
+                                        Continuing will permanently delete the
+                                        user and all of their associated
+                                        personal data from the server. Content
+                                        generated by the user will not be
+                                        affected.
+                                    </DialogDescription>
+                                </DialogHeader>
+                                <div className="flex justify-end space-x-2">
+                                    <Button
+                                        variant="outline"
+                                        onClick={() =>
+                                            setConfirmDeleteOpen(false)
+                                        }
+                                    >
+                                        Cancel
+                                    </Button>
+                                    <Button
+                                        variant="destructive"
+                                        disabled={deleteBusy}
+                                        onClick={async () => {
+                                            setDeleteBusy(true);
+                                            await handleUserDelete(user.id);
+                                            // row.
+                                            setDeleteBusy(false);
+                                            setConfirmDeleteOpen(false);
+                                        }}
+                                    >
+                                        {deleteBusy ? (
+                                            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                                        ) : null}
+                                        Delete
+                                    </Button>
+                                </div>
+                            </DialogContent>
+                        </Dialog>
+                    </>
+                );
+            },
+        },
+    ];
+
+    return <DTable data={data} columns={columns} className="w-full" />;
+}
+
+interface DirectoryTableProps<TData, TValue> {
     data: TData[];
     columns: ColumnDef<TData, TValue>[];
     className?: string;
 }
 
-export const columns: ColumnDef<UserNoPassword>[] = [
-    {
-        accessorKey: "firstname",
-        header: "First Name",
-    },
-    {
-        accessorKey: "lastname",
-        header: "Last Name",
-    },
-    {
-        accessorKey: "email",
-        header: "Email",
-    },
-    {
-        accessorKey: "phone",
-        header: "Phone",
-    },
-    {
-        id: "actions",
-        cell: ({ row }) => {
-            const user = row.original;
-
-            return (
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem
-                            onClick={() =>
-                                navigator.clipboard.writeText(user.id)
-                            }
-                        >
-                            Copy User ID
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <Link to={`/user/${user.id}`}>
-                            <DropdownMenuItem>View Profile</DropdownMenuItem>
-                        </Link>
-
-                        <DropdownMenuItem>Edit User</DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem className="font-bold text-red-600">
-                            Delete
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            );
-        },
-    },
-];
-
-export default function DirectoryTable<TData, TValue>({
+function DTable<TData, TValue>({
     data,
     columns,
     className,
-}: DataTableProps<TData, TValue>) {
+}: DirectoryTableProps<TData, TValue>) {
     const [sorting, setSorting] = useState<SortingState>([]);
 
     const table = useReactTable({
