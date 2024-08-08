8ed795e01c2959ec297654fda746754ce152388a
Jason Ballinger <jasonballinger_dev@yahoo.com>
2024-08-07 20:26:10 -0700

    theme provider setup

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -3,11 +3,15 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import { AuthProvider } from "./context/auth.tsx";
+import { ThemeProvider } from "./context/theme.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <AuthProvider>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </AuthProvider>
+    // TODO: Add theme toggle
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <AuthProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </AuthProvider>
+    </ThemeProvider>
 );
